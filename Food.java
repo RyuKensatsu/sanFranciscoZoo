07f37e91a435cb305a9e49edcf193a63d04ab7d7
Quenntinne <wilbauxq@gmail.com>
2018-12-13 16:11:53 +0100

    Add getter and setter and constructor of food

--- Food.java
+++ Food.java
@@ -4,4 +4,34 @@ public class Food {
     private String foodType;
     private Integer hungering;
     private Integer energying;
+
+    public String getFoodType() {
+        return foodType;
+    }
+
+    public void setFoodType(String foodType) {
+        this.foodType = foodType;
+    }
+
+    public Integer getHungering() {
+        return hungering;
+    }
+
+    public void setHungering(Integer hungering) {
+        this.hungering = hungering;
+    }
+
+    public Integer getEnergying() {
+        return energying;
+    }
+
+    public void setEnergying(Integer energying) {
+        this.energying = energying;
+    }
+
+    public Food(String foodType, Integer hungering, Integer energying) {
+        this.foodType = foodType;
+        this.hungering = hungering;
+        this.energying = energying;
+    }
 }
