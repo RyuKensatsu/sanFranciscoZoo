db423b083af2d76cc14dd718ad38a2a24dfb3302
Quenntinne <wilbauxq@gmail.com>
2018-12-14 11:45:00 +0100

    Add a creation of an soigneur and stats for this soigneur

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
 package zoo;
 
+import java.util.*;
+
 public class Main {
-    private static void main(String[] args){
-        AnimalManagerImpl am = new AnimalManager();
+    public static void main(String[] args){
+        List<Soigneurs> soigneurs = new ArrayList<>();
+        SoigneurManagerIntf sm = new SoigneurManager();
+        System.out.println("Bienvenue à toi dans ton zoo !\nNous allons créer un premier soigneur pour commencer :)");
+        Soigneurs bily = sm.newSoigneur();
+        soigneurs.add(bily);
+        sm.getStats(soigneurs.get(0));
+        AnimalManagerIntf am = new AnimalManager();
         Animal bob = am.adoptAnimal();
 
     }
